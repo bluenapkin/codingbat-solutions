684c3721927907197fa50e2d837f19f6f8276237
Nawaf abdullah <nawaf@example.com>
2025-07-24 11:12:39 +0400

    Delete sleepIn.java

--- src/sleepIn.java
+++
@@ -1,9 +0,0 @@
-public class sleepIn {
-    public boolean sleepIn(boolean weekday, boolean vacation) {
-        if (!weekday || vacation){
-            return true;
-        }
-        return false;
-    }
-
-}
