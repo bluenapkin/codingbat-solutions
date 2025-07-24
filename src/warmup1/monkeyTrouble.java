cf2ac691b0e336ffb6d3d217de3356f832d3b4be
Nawaf abdullah <nawaf@example.com>
2025-07-24 11:08:45 +0400

    all files move to Warmup-1

--- src/warmup1/monkeyTrouble.java
+++ src/warmup1/monkeyTrouble.java
@@ -1,4 +1,13 @@
 package warmup1;
 
 public class monkeyTrouble {
+    public boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
+        if(aSmile && bSmile){
+            return true;
+        }
+        if(!aSmile && !bSmile){
+            return true;
+        }
+        return false;
+    }
 }
---
+++ src/warmup1/sleepIn.java
@@ -0,0 +1,11 @@
+package warmup1;
+
+public class sleepIn {
+    public boolean sleepIn(boolean weekday, boolean vacation) {
+        if (!weekday || vacation){
+            return true;
+        }
+        return false;
+    }
+
+}
