2077d41ae36e762f7cd98bf856b06dadb91bbed4
Nawaf abdullah <nawaf@example.com>
2025-07-24 11:31:18 +0400

    added sleepIn.java done

--- src/warmup1/sleepIn.java
+++ src/warmup1/sleepIn.java
@@ -1,7 +1,7 @@
 package warmup1;
 
 public class sleepIn {
-    public boolean sleepIn(boolean weekday, boolean vacation) {
+    public boolean sleepIn(boolean weekdayy, boolean vacation) {
         if (!weekday || vacation){
             return true;
         }
