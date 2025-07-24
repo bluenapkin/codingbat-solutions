2175814c83448c63332008125f6b138e62720efc
Nawaf abdullah <nawaf@example.com>
2025-07-24 11:32:15 +0400

    fix problem in file sleepIn.java done

--- src/warmup1/sleepIn.java
+++ src/warmup1/sleepIn.java
@@ -2,7 +2,7 @@ package warmup1;
 
 public class sleepIn {
     public boolean sleepIn(boolean weekdayy, boolean vacation) {
-        if (!weekday || vacation){
+        if (!weekdayy || vacation){
             return true;
         }
         return false;
