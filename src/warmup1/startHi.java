381f06f9835dc9e9850823344e9261bb446f5359
Nawaf abdullah <nawaf@example.com>
2025-07-27 11:26:54 +0400

    Add startHi.java to warmup1.

---
+++ src/warmup1/startHi.java
@@ -0,0 +1,16 @@
+package warmup1;
+
+public class startHi {
+    public boolean startHi(String str) {
+        if (str.length() < 2) return false;
+
+        String firstTwo = str.substring(0, 2);
+
+        if (firstTwo.equals("hi")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
