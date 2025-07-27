5c829233d3450864f8b921ae856c5a48a4f71382
Nawaf abdullah <nawaf@example.com>
2025-07-27 11:53:36 +0400

    Add loneTeen.java to warmup1.  We'll say that a number is teen if it is in the range 13..19 inclusive. Given 2 int values, return true if one or the other is teen, but not both.

---
+++ src/warmup1/loneTeen.java
@@ -0,0 +1,11 @@
+package warmup1;
+
+public class loneTeen {
+    public boolean loneTeen(int a, int b) {
+        boolean aTeen = (a >= 13 && a <= 19);
+        boolean bTeen = (b >= 13 && b <= 19);
+
+        return (aTeen && !bTeen) || (!aTeen && bTeen);
+    }
+
+}
