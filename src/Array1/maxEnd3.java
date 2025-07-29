f8defd4f6d62139221c68fe4beb3424927c99095
Nawaf abdullah <nawaf@example.com>
2025-07-29 08:37:12 +0400

    Add maxEnd3 to folder Array1.

---
+++ src/Array1/maxEnd3.java
@@ -0,0 +1,9 @@
+package Array1;
+
+public class maxEnd3 {
+    public int[] maxEnd3(int[] nums) {
+        int max = Math.max(nums[0], nums[2]);
+        return new int[] {max, max, max};
+    }
+
+}
